3307b7b10956b38a58924f2763436f11a9cabfa6
Per A. Bergersen <pab2222@hotmail.com>
2022-11-14 21:53:29 +0100

    Add project files.

---
+++ NutsMini/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutsMini
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// teller antall siffer i et tall
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        static int Len(int num)
+        {
+            int digits = 1;
+            while ((num /= 10) != 0)
+                digits++;
+            return digits;
+        }
+
+        /// <summary>
+        /// teller antall siffer i et tall - muhannads versjon
+        /// hva må utbedres på denne tilsynelatende perfekte versjonen?
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        static int len(int num)
+        {
+            int len = 0;
+            for (int i = 1; i < num; i *= 10)
+            {
+                len++;
+            }
+            return len;
+        }
+    }
+}
